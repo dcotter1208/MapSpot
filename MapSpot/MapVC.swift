31d84109b507cbada5d147cb8932d88f3e9fa4ba
Donovan Cotter <donovan.cotter@yahoo.com>
2016-02-17 18:52:41 -0500

    first commit

---
+++ MapSpot/MapVC.swift
@@ -0,0 +1,29 @@
+//
+//  MapVC.swift
+//  MapSpot
+//
+//  Created by Donovan Cotter on 2/17/16.
+//  Copyright © 2016 DonovanCotter. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+
+class MapVC: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
