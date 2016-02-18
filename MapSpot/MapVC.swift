7a77e2ba4ddaaa1dc3ce4cc76093f3a46bc2837c
Donovan Cotter <donovan.cotter@yahoo.com>
2016-02-18 13:05:55 -0500

    implemented CLLocation

--- MapSpot/MapVC.swift
+++ MapSpot/MapVC.swift
@@ -11,20 +11,56 @@ import MapKit
 
 
 
-class MapVC: UIViewController {
+class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
+    
+    private var locationManager:CLLocationManager?
+    private let distanceSpan:Double = 800
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mapView.showsPointsOfInterest = false
-        
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        
+        if let mapView = self.mapView
+        {
+            mapView.delegate = self
+            mapView.showsPointsOfInterest = false
+
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool)
+    {
+        if locationManager == nil {
+            locationManager = CLLocationManager()
+            
+            locationManager!.delegate = self
+            locationManager!.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+            locationManager!.requestAlwaysAuthorization()
+            locationManager!.distanceFilter = 50 // Don't send location updates with a distance smaller than 50 meters between them
+            locationManager!.startUpdatingLocation()
+            mapView.showsUserLocation = true
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        if let mapView = self.mapView {
+            let region = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, distanceSpan, distanceSpan)
+            mapView.setRegion(region, animated: true)
+        }
     }
 
 
